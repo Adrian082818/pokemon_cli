09957e5aa6715fe473958196fa106b3c3eab4221
Adrian Mohabir <mohabiradrian@gmail.com>
2020-01-16 01:54:06 +0000

    modified files

---
+++ lib/pokemon_cli/api.rb
@@ -0,0 +1,30 @@
+require 'rest-client'
+require 'json'
+require 'pry'
+require_relative './pokemon'
+
+
+class API 
+  URL = "https://pokeapi.co/api/v2/"
+  
+  def get_all_pokemon(page=nil)
+    if page 
+      pokemon_info = RestClient.get(page)
+    else 
+   pokemon_info = RestClient.get("https://pokeapi.co/api/v2/pokemon")
+ end 
+   pokemons_array = JSON.parse(pokemon_info.body)["results"]
+   
+   pokemons_array.each do |pokemon|
+  Pokemon.new(pokemon)
+    end
+    
+    next_page = JSON.parse(pokemon_info.body)["next"]
+    if next_page 
+      get_all_pokemon(next_page)
+    end 
+end 
+
+
+end 
+# binding.pry 
---
+++ lib/pokemon_cli/pokemon.rb
@@ -0,0 +1,21 @@
+class Pokemon 
+  
+  attr_accessor :name, :weight, :height, :type, :gender
+  
+  @@all = []
+  
+  def initialize(attr_hash)
+    attr_hash.each do |key, value|
+      self.send("#{key}=", value) if self.respond_to?("#{key}=")
+    end 
+    self.save
+  end 
+  
+  def save
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+end 
